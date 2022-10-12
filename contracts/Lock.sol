99cb84e2362dfe2ea8bf256c288a8a4001955514
Jason Novack <jasonnovack@Jasons-MacBook-Pro.local>
2022-10-11 21:50:47 -0400

    added ZSU contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
+
 contract Lock {
     uint public unlockTime;
     address payable public owner;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,28 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await hre.ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // await lock.deployed();
 
-  await lock.deployed();
+  const [deployer] = await ethers.getSigners();
+
+  const ZSU = await hre.ethers.getContractFactory("ZSUStudentOrientation");
+  const zsu = await ZSU.deploy("Zombie State Test","ZST","https://zst.test","https://credits.zst.test",deployer.address);
+  await zsu.deployed();
+
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `ZST deployed to ${zsu.address}`
   );
 }
 
