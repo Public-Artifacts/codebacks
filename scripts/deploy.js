4bd80cdcf8b8b5e8b97d58c6a4021b8fff42ecd9
Jason Novack <jasonnovack@Jasons-MacBook-Pro.local>
2022-10-11 20:23:45 -0400

    restored await deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,7 +16,7 @@ async function main() {
   const Lock = await hre.ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  // await lock.deployed();
+  await lock.deployed();
 
   console.log(
     `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
