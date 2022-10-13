71ccd132e1e33bf4bf513cd18bb17130175822be
Jason Novack <jasonnovack@Jasons-MacBook-Pro.local>
2022-10-13 17:16:11 -0400

    added script to call forkback function with tip

---
+++ scripts/use_with_tip.js
@@ -0,0 +1,38 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const [tester] = await ethers.getSigners();
+
+  const Generator = await hre.ethers.getContractFactory("StudentIDGenerator");
+  const generator = await Generator.deploy({
+    gasLimit: 3e7
+  });
+  await generator.deployed();
+
+  console.log(`Student ID Generator deployed to ${generator.address}`);
+
+  console.log(`Generator Forkback balance is now ${await generator.balance()} ETH`);
+  
+  options = {
+    value: ethers.utils.parseEther('2')
+  };
+
+  const studentId = await generator.setStudentId(3, generator.address, options);
+
+  console.log(`Your Student ID is ${studentId}`);
+
+  console.log(`Generator Forkback balance is now ${await generator.balance()} ETH`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
