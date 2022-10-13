a883639fbce5721cab352003c45b0e000b8721ca
Jason Novack <jasonnovack@Jasons-MacBook-Pro.local>
2022-10-13 16:19:51 -0400

    added script to test tipping ETH to the forkback

---
+++ scripts/tip_eth.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const [tester] = await ethers.getSigners();
+
+  const Generator = await hre.ethers.getContractFactory("StudentIDGenerator");
+  const generator = await Generator.deploy({
+    gasLimit: 3e7
+  });
+  await generator.deployed();
+
+  console.log(`Generator Forkback balance is now ${await generator.balance()} ETH`);
+  
+  tx = {
+    to: generator.address,
+    value: ethers.utils.parseEther('2')
+  };
+
+  const tipEthTx = await tester.sendTransaction(tx);
+
+  console.log(`ETH tip sent`);
+
+  console.log(`Generator Forkback balance is now ${await generator.balance()} ETH`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
