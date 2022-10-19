e6ae5b9f1b24e3d1adaa2f16b448e13c16b9e4bb
Jason Novack <jasonnovack@Jasons-MacBook-Pro.local>
2022-10-19 18:03:46 -0400

    removed extraneous console library

--- contracts/Forkback_StudentIDGenerator.sol
+++ contracts/Forkback_StudentIDGenerator.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
